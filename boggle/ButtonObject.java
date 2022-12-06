a647b46a8d11e72946be6db5949bd33e4514634c
Hyeonbinchun <97205988+Hyeonbinchun@users.noreply.github.com>
2022-12-05 21:13:21 -0500

    Feature:Highest

--- boggle/ButtonObject.java
+++ boggle/ButtonObject.java
@@ -1,19 +1,4 @@
 package boggle;
-
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 public interface ButtonObject {
 
     public void press();
